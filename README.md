8035db81867e3bae28bbca31f84a63cc56d9d587
Sergey Tokarev <sergey.zoomaster@gmail.com>
2021-10-27 21:50:54 +0700

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# Neuro Busido
-
-просто небольшое приложение на React App
-
-тексты отсюда - [https://kelijah.livejournal.com/292717.html](https://kelijah.livejournal.com/292717.html)
--- src/services/actions/data/index.ts
+++
@@ -1,8 +0,0 @@
-export enum DataActionType {
-    INIT_DATA = 'INIT_DATA',
-    NEXT_SENTENCE = 'NEXT_SENTENCE',
-}
-
-export type DataAction =
-    | { type: DataActionType.INIT_DATA, sentences: string[] }
-    | { type: DataActionType.NEXT_SENTENCE }
